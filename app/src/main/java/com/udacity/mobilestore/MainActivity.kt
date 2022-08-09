1764e5f09b5a7b06845119a589019e0864d6c5d1
ksdc-anandhid <Anandhi.Dharmarajan@ksdc.co.za>
2022-08-09 21:12:52 +0200

    Initial Commit

---
+++ app/src/main/java/com/udacity/mobilestore/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.udacity.mobilestore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import timber.log.Timber
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var appBarConfiguration : AppBarConfiguration
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        findViewById<Toolbar>(R.id.toolbar)
+            .setupWithNavController(navController, appBarConfiguration)
+        Timber.plant(Timber.DebugTree())
+    }
+}
