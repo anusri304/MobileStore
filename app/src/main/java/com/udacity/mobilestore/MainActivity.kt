d65438d45fbb171e696f38c786ccf0687600ee61
ksdc-anandhid <Anandhi.Dharmarajan@ksdc.co.za>
2022-08-10 11:49:36 +0200

    Added Databinding

--- app/src/main/java/com/udacity/mobilestore/MainActivity.kt
+++ app/src/main/java/com/udacity/mobilestore/MainActivity.kt
@@ -3,16 +3,21 @@ package com.udacity.mobilestore
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.widget.Toolbar
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
+import com.udacity.mobilestore.databinding.ActivityMainBinding
+import com.udacity.mobilestore.databinding.FragmentLoginBinding
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
     private lateinit var appBarConfiguration : AppBarConfiguration
+    private var binding: ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
         val navController = this.findNavController(R.id.myNavHostFragment)
         appBarConfiguration = AppBarConfiguration(navController.graph)
         findViewById<Toolbar>(R.id.toolbar)
