8d926643ea3abaf3585781591576ce6ae5744d1a
ksdc-anandhid <Anandhi.Dharmarajan@ksdc.co.za>
2022-08-12 15:32:25 +0200

    Added code for Mobile List

--- app/src/main/java/com/udacity/mobilestore/MobileFragment.kt
+++ app/src/main/java/com/udacity/mobilestore/MobileFragment.kt
@@ -6,6 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import com.udacity.mobilestore.databinding.FragmentMobileBinding
+import com.udacity.mobilestore.databinding.FragmentWelcomeBinding
 
 class MobileFragment : Fragment() {
 
@@ -15,16 +19,33 @@ class MobileFragment : Fragment() {
 
     private lateinit var viewModel: MobileViewModel
 
+    lateinit var binding: FragmentMobileBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_mobile, container, false)
+      binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_mobile,
+            container,
+            false
+        )
+        viewModel = ViewModelProvider(this).get(MobileViewModel::class.java)
+        // Specify the current activity as the lifecycle owner of the binding. This is used so that
+        // the binding can observe LiveData updates
+        binding.setLifecycleOwner(this)
+
+
+        viewModel.mobiles.observe(viewLifecycleOwner, Observer { mobileList ->
+            println(mobileList.get(0))
+        })
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MobileViewModel::class.java)
+
     }
 
 }
